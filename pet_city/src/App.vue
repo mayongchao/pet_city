3c55098a97801a59e26c868c8e2a85a0b014a945
mayongcaho <mayongchaovip@163.com>
2017-08-11 12:37:38 +0800

    品牌特卖

--- pet_city/src/App.vue
+++ pet_city/src/App.vue
@@ -4,26 +4,28 @@
 
             <pet-header></pet-header>
 
-            <ul class="tab clearfix">
-                <li class="tab-item">
-                    <router-link to="/home" class="tab-item-list">首页</router-link>
-                </li>
-                <li class="tab-item">
-                    <router-link to="/catFood" class="tab-item-list">猫粮</router-link>
-                </li>
-                <li class="tab-item">
-                    <router-link to="/sale" class="tab-item-list">特卖</router-link>
-                </li>
-                <li class="tab-item">
-                    <router-link to="/canSupermarket" class="tab-item-list">罐头超市</router-link>
-                </li>
-                <li class="tab-item">
-                    <router-link to="/video" class="tab-item-list">潮品视频</router-link>
-                </li>
-                <li class="tab-item">
-                    <router-link to="/catClassRome" class="tab-item-list">猫咪课堂</router-link>
-                </li>
-            </ul>
+           <div class="navbarScroll" ref="navbar">
+               <ul class="tab clearfix" ref="tabs">
+                   <li class="tab-item" ref="tabItem">
+                       <router-link to="/home" class="tab-item-list ">首页</router-link>
+                   </li>
+                   <li class="tab-item">
+                       <router-link to="/catFood" class="tab-item-list active">猫粮</router-link>
+                   </li>
+                   <li class="tab-item">
+                       <router-link to="/sale" class="tab-item-list">特卖</router-link>
+                   </li>
+                   <li class="tab-item">
+                       <router-link to="/canSupermarket" class="tab-item-list">罐头超市</router-link>
+                   </li>
+                   <li class="tab-item">
+                       <router-link to="/video" class="tab-item-list">潮品视频</router-link>
+                   </li>
+                   <li class="tab-item">
+                       <router-link to="/catClassRome" class="tab-item-list">猫咪课堂</router-link>
+                   </li>
+               </ul>
+           </div>
 
         </div>
 
@@ -34,12 +36,41 @@
     </div>
 </template>
 <script>
+    import BScroll from 'better-scroll'
     import header from './components/header/header.vue'
     import footernav from './components/footernav/footernav.vue'
     export default{
         data(){
             return {}
         },
+        created(){
+
+          this.$nextTick(()=>{
+              let tabchild = this.$refs.tabs.childElementCount
+              console.log(tabchild)
+              let itemLength = this.$refs.tabItem.clientWidth
+              console.log(itemLength)
+              this.$refs.tabs.style.width=itemLength * tabchild +'px'
+              console.log(this.$refs.tabs.style.width)
+
+              if(!this.navScroll){
+                  this.navScroll = new BScroll(this.$refs.navbar,{
+                      click:true,
+                      scrollX: 'true'
+                  })
+              }else {
+                  this.navScroll.refresh()
+              }
+
+          })
+        },
+       /* mounted(){
+            setTimeout(()=>{
+                let tabchild = this.$refs.tabs
+                console.log(tabchild)
+            },300)
+
+        },*/
         components: {
             "pet-header": header,
             footernav
@@ -63,31 +94,33 @@
      position relative
      z-index 1000
      background-color #ffffff
-     .tab
-        width 120%
-        height 36px
-       .tab-item
-          float left
-          line-height 36px
-          width 75px
-          text-align center
-         a
-           font-size 14px
-           color #666
-         a:hover
-           color #e73f85
-         .tab-item-list:hover:after
-          content ""
-          display block
-          height 2px
-          background-color #e73f85
-         .active
-           color #e73f85
-         .active:after
-           content ""
-           display block
-           width 100%
-           height 2px
-           background-color #e73f85
+     .navbarScroll
+       .tab
+          /*width 120%*/
+          height 36px
+         .tab-item
+            float left
+            line-height 36px
+            width 75px
+            text-align center
+           a
+             font-size 14px
+             color #666
+           a:hover
+             color #e73f85
+           .tab-item-list:hover:after
+             content ""
+             display block
+             height 2px
+             background-color #e73f85
+           .active
+             color #e73f85
+           .active:after
+             content ""
+             display block
+             width 100%
+             height 2px
+             background-color #e73f85
+             margin-top -5px
 
 </style>
--- pet_city/src/components/sale/sale.vue
+++ pet_city/src/components/sale/sale.vue
@@ -1,15 +1,253 @@
 <template>
-    <div>
-        特卖
-    </div>
+    <!--<scroller>-->
+        <div class="brand">
+
+            <scroller>
+                <div class="brand-head">
+                    <div class="brand-wrapper">
+                <span class="icon-left">
+                    <img src="./personal-bico1.png" alt="">
+                </span>
+                        <span class="headTitle">
+                    品牌特卖
+                </span>
+                        <span class="icon-right">
+                    <img src="./imgtk-new.png" alt="">
+                </span>
+                    </div>
+                </div>
+
+                <div class="epetApp">
+                    <ul class="appItem">
+                        <li class="appList">
+                            <a href="javascript">
+                                <img src="./head_icon1.png" alt="">
+                                <p class="text">首页</p>
+                            </a>
+                        </li>
+                        <li class="appList">
+                            <a href="javascript">
+                                <img src="./head_icon2.png" alt="">
+                                <p class="text">商品分类</p>
+                            </a>
+                        </li>
+                        <li class="appList">
+                            <a href="javascript">
+                                <img src="./head_icon4.png" alt="">
+                                <p class="text">购物车</p>
+                            </a>
+                        </li>
+                        <li class="appList">
+                            <a href="javascript">
+                                <img src="./head_icon5.png" alt="">
+                                <p class="text">我的e宠</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="brand-banner">
+                    <img src="./banner.jpg" alt="">
+                </div>
+
+                <div class="page-navbar">
+                    <!--<div class="page-title">Navbar</div>-->
+                    <mt-navbar class="page-part" v-model="selected">
+                        <mt-tab-item id="1">选项一</mt-tab-item>
+                        <mt-tab-item id="2">选项二</mt-tab-item>
+                        <mt-tab-item id="3">选项三</mt-tab-item>
+                        <mt-tab-item id="4">选项四</mt-tab-item>
+                    </mt-navbar>
+
+                    <div>
+                        <!--<mt-cell class="page-part" title="当前选中">{{ selected }}</mt-cell>-->
+                    </div>
+
+                    <mt-tab-container v-model="selected" class="page-navlist">
+
+                        <mt-tab-container-item id="1" class="page-item">
+                            <ul class="imgItem">
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./banner1.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./banner2.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./banner3.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./banner4.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                            </ul>
+                        </mt-tab-container-item>
+
+                        <mt-tab-container-item id="2">
+                            <ul class="imgItem">
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./1.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./2.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./3.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./4.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                            </ul>
+                        </mt-tab-container-item>
+
+                        <mt-tab-container-item id="3">
+                            <ul class="imgItem">
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./banner1.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./banner2.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./banner3.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./banner4.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                            </ul>
+                        </mt-tab-container-item>
+
+                        <mt-tab-container-item id="4">
+                            <ul class="imgItem">
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./1.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./2.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./3.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                                <li class="imgList">
+                                    <a href="javascript">
+                                        <img src="./4.jpg" alt="" class="imgbanner">
+                                    </a>
+                                </li>
+                            </ul>
+                        </mt-tab-container-item>
+
+                    </mt-tab-container>
+                </div>
+
+            </scroller>
+
+        </div>
+    <!--</scroller>-->
+
 </template>
 <script>
     export default{
-        data(){
-            return {}
+        name: 'page-navbar',
+        data() {
+            return {
+                selected: '1'
+            };
         }
     }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 
+  .brand
+    width 100%
+    height 100%
+    background-color #ffffff
+    position fixed
+    top 0
+    left 0
+    z-index 1000
+    .brand-head
+      .brand-wrapper
+        height 50px
+        padding 0 15px
+        line-height 50px
+        text-align center
+        vertical-align middle
+          /*此处可能涉及到1px像素的问题*/
+        border-bottom 1px solid #d7d7d7
+        .icon-left
+          float left
+          img
+            vertical-align middle
+            width 10px
+        .headTitle
+          font-size 18px
+        .icon-right
+          float right
+          width 20px
+          height 20px
+          img
+            width 100%
+            vertical-align middle
+    .epetApp
+      .appItem
+        .appList
+          padding 10px 0
+          width 25%
+          text-align center
+          float left
+          a
+            display block
+            text-align center
+            font-size 0
+            img
+              height 30px
+
+            .text
+              font-size 14px
+              color #666
+              text-align center
+
+    .brand-banner
+      img
+        width 100%
+    .page-navbar
+      .imgItem
+        background-color #F3F4F5
+        .imgList
+          margin-top 10px
+          a
+            display block
+            width 100%
+            font-size 0
+            img
+              width 100%
 </style>
